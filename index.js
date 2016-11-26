924d3c078e4211e81c00ce8cb0e1bde45e7b2375
PLD <pierre-louis@dourneau.me>
2016-11-26 22:41:06 +0100

    Set basic structure

--- index.js
+++ index.js
@@ -18,7 +18,34 @@ let svg = d3.select('body')
     .attr('class', 'graph')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-
 d3.csv('labor.csv', (err, data) => {
   console.log(data);
+
+  total = totalLaborsPer(data, -1);
+  month = totalLaborsPer(data, 31);
+  week = totalLaborsPer(data, 7);
+  day = totalLaborsPer(data, 1);
+
+  console.log("total", total);
+  console.log("month", month);
+  console.log("week", week);
+  console.log("day", day);
+
+  categoriesByTotal = categorizedLaborsPer(data, -1);
+  categoriesByMonth = categorizedLaborsPer(data, 31);
+  categoriesByWeek = categorizedLaborsPer(data, 7);
+  categoriesByDay = categorizedLaborsPer(data, 1);
+
+  console.log("categoriesByTotal", categoriesByTotal);
+  console.log("categoriesByMonth", categoriesByMonth);
+  console.log("categoriesByWeek", categoriesByWeek);
+  console.log("categoriesByDay", categoriesByDay);
 });
+
+function totalLaborsPer (data, days) {
+
+}
+
+function categorizedLaborsPer (data, days) {
+
+}
