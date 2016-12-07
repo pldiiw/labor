397bcbaa70e444662e9c729031e6f704196d1738
PLD <pierre-louis@dourneau.me>
2016-12-07 13:50:12 +0100

    Remove unnecessary backslashes and add labors

--- labor.zsh
+++ labor.zsh
@@ -1,31 +1,33 @@
 #!/usr/bin/zsh
 
 echo "Total labors done by $1:"
-total=`cat labor.csv | \
-  grep $1 | \
-  sed 's/2\([a-z]\+\)/\1-\1/g' | \
+
+total=`cat labor.csv |
+  grep $1 |
+  sed 's/2\([a-z]\+\)/\1-\1/g' |
   awk -F, -f labor.awk`
+
 echo $total | sort | uniq -c
 echo "Or `echo $total | wc -l` labors."
 
 echo
 echo "Labors per month done by $1:"
 
-months=(`cat labor.csv | \
-  tail -n +2 | \
-  awk -F, '{ print $1 }' | \
-  awk -F"-" '{ print $2}' | \
-  sort | \
-  uniq | \
+months=(`cat labor.csv |
+  tail -n +2 |
+  awk -F, '{ print $1 }' |
+  awk -F"-" '{ print $2}' |
+  sort |
+  uniq |
   tr '\n ' ' '`)
 
 for month in $months; do
   echo "On `date -d 2016-$month-01 +%B`:"
 
-  labors=`cat labor.csv | \
-  grep $1 | \
-  grep "2016-$month" | \
-  sed 's/2\([a-z]\+\)/\1-\1/g' | \
+  labors=`cat labor.csv |
+  grep $1 |
+  grep "2016-$month" |
+  sed 's/2\([a-z]\+\)/\1-\1/g' |
   awk -F, -f labor.awk`
 
   echo $labors | sort | uniq -c
@@ -36,23 +38,23 @@ done
 echo
 echo "Labors per week done by $1:"
 
-weeks=(`cat labor.csv | \
-  tail -n +2 | \
-  awk -F, '{ print $1 }' | \
-  xargs -n 1 -I {} date -d {} +%W | \
-  sort | \
-  uniq | \
+weeks=(`cat labor.csv |
+  tail -n +2 |
+  awk -F, '{ print $1 }' |
+  xargs -n 1 -I {} date -d {} +%W |
+  sort |
+  uniq |
   tr '\n' ' '`)
 
 for week in $weeks; do
   echo "On week #$week:"
 
-  labors=`cat labor.csv | \
-    grep $1 | \
+  labors=`cat labor.csv |
+    grep $1 |
     awk -F, -v week=$week \
-      '{ "date -d "$1" +%W" | getline cw; if ( cw == week ) print $0 }' | \
-  sed 's/2\([a-z]\+\)/\1-\1/g' | \
-  awk -F, -f labor.awk`
+      '{ "date -d "$1" +%W" | getline cw; if ( cw == week ) print $0 }' |
+    sed 's/2\([a-z]\+\)/\1-\1/g' |
+    awk -F, -f labor.awk`
 
   echo $labors | sort | uniq -c
   echo "Or `echo $labors | wc -l` labors."
